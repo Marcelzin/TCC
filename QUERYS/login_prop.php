a6c73a0a791dda7991fe2676687f9c8d7e396dc1
Marcel <contatomarcelpereira@gmail.com>
2023-09-01 08:26:08 -0300

    Ajustes login prop
    Signed-off-by: Marcel <contatomarcelpereira@gmail.com>

--- QUERYS/login_prop.php
+++ QUERYS/login_prop.php
@@ -1,21 +1,31 @@
 <?php
 include_once('config.php');
 
+$response = array(); // Array para armazenar a resposta
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-    // Consulta no banco de dados para verificar se o usuário existe e tem o nível de acesso adequado
-    $query = "SELECT * FROM usuario WHERE email = '$email' AND senha = '$senha' AND nivel_acesso = 'Proprietário'";
+    // Consulta no banco de dados para verificar se o usuário existe e tem o nível de acesso adequado e a senha está correta
+    $query = "SELECT * FROM usuario WHERE email = '$email' AND nivel_acesso = 'Proprietário' AND senha = '$senha'";
     $result = mysqli_query($conexao, $query);
 
     if (mysqli_num_rows($result) == 1) {
-        // Usuário válido, redireciona para a página principal ou faz outras ações necessárias
-        header("Location: /TCC/PAGES/home.html");
-        exit();
+        // Usuário encontrado, senha correta
+        $response['status'] = 'success';
+        $response['message'] = 'Login bem-sucedido! Redirecionando...';
     } else {
-        // Usuário inválido, exibe uma mensagem de erro ou redireciona para a página de login novamente
-        echo "Email, senha ou nível de acesso inválidos";
+        // Usuário não encontrado ou senha incorreta
+        $response['status'] = 'error';
+        $response['message'] = 'Email não cadastrado como proprietário ou senha incorreta.';
     }
+} else {
+    // Método de requisição inválido (não é POST)
+    $response['status'] = 'error';
+    $response['message'] = 'Método de requisição inválido.';
 }
+
+// Retorna a resposta em formato JSON
+echo json_encode($response);
 ?>
